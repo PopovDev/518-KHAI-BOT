c8c0c974020fbe80618efc93a297edea76a75404
Popov Andrey <a.a.popov@student.khai.edu>
2022-09-21 20:20:26 +0300

    test of url

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-import config as conf
+import config
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+import logging
 
-print(conf.BOT_KEY)
+print(config.HEROKU_APP_NAME)
+print(config.WEBHOOK_URL)
--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 import os
 
 BOT_KEY = os.getenv('BOT_KEY')
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+WEBHOOK_URL = f'https://{HEROKU_APP_NAME}.herokuapp.com/webhook/{BOT_KEY}'
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT'))
